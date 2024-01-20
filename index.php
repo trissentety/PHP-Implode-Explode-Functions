c5dd81d730aebc892cc9ea40421188bc4d6e09db
Nicholas Roberts <82165867+trissentety@users.noreply.github.com>
2024-01-20 16:53:22 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$name = "Spongebob Squarepants";
+$full_name = explode(" ", $name); //full name is array. Take each portion as an array and place them as new elements inside array
+foreach ($full_name as $name) {
+    echo $name . "<br>";
+}
+//returns
+//Spongebob
+//Squarepants
+
+$aUsername = array("Sponge", "bob", " Square", "pants");
+$aUsername = implode(" ", $aUsername);
+echo $aUsername . "<br>";
+//returns
+//Sponge bob Square pants
+
+$aUsername1 = array("Sponge", "bob", " Square", "pants");
+$aUsername1 = implode($aUsername1);
+echo $aUsername1 . "<br>";
+//returns
+//Spongebob Squarepants
